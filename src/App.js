10e5f07f6a243fc165513764ccce8ff7848b0248
Srinivas kumar <seenukumar124@gmail.com>
2022-10-28 02:58:08 +0000

    React JS Conditional Rendering

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {isLoggedIn && <button>Logout</button>}
+        {!isLoggedIn && <button>Login</button>}
+      </div>
+    )
+  }
+}
+
+export default App
